ae885e4e1e80c97c401d796e3b3d9b73cbbc08ae
Ilia Kliukin <klyukin.ilia@gmail.com>
2026-01-08 16:44:11 -0800

    add where to update

--- update_functions.py
+++ update_functions.py
@@ -4,11 +4,15 @@ from .errors import*
 #UPDATE tablename SET isRead = 1;
 def update(command, database_name, user):
     table_name = command.split(" ")[1]
-    changes = command.split(" SET ")[1].split(", ")
+    changes = command.split(" SET ")[1].split(" WHERE ")[0].split(", ")
+    where_statement = command.split(" WHERE ")[1]
+    where_column = where_statement.split(" = ")[0]
+    where_value = where_statement.split(" = ")[1]
     database = get_database(database_name, user)
     for record in database[table_name]:
         for i in changes:
             column = i.split(" = ")[0]
             new_value = i.split(" = ")[1]
-            record[column] = new_value
+            if record[where_column] == where_value:
+                record[column] = new_value
     save_database(database, database_name, user)
